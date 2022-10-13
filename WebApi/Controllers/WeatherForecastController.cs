a1a3e9677f41d8b1862e8e468ef0954f269d2fbe
Oladeji Mujib <oladejimujeeb@yahoo.com>
2022-10-13 15:43:36 +0100

    Adding IdentityServer UI

--- WebApi/Controllers/WeatherForecastController.cs
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -33,14 +33,27 @@ namespace WebApi.Controllers
         [ProducesResponseType((int)HttpStatusCode.Unauthorized, Type = typeof(ResponseModel))]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            try 
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                //checking claims
+                var claims = User.Claims;
+                var rng = new Random();
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+            }
+
+            catch(Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return Enumerable.Empty<WeatherForecast>();
+            }
+
+            
         }
     }
 }
